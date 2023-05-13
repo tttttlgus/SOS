b518991f99e6ee655d338f51349abcb79ad96f22
jaeyoon0330 <91931755+jaeyoon0330@users.noreply.github.com>
2023-05-14 00:34:21 +0900

    데이터베이스 연결해보기

---
+++ app/src/main/java/com/example/mp_snow/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mp_snow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//https://www.youtube.com/watch?v=NJgolOfKcYE
+public class MainActivity extends AppCompatActivity {
+
+//    FirebaseStorage storage = FirebaseStorage.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
